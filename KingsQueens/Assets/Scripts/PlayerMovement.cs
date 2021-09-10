aaa2d1890efecf691e2d9754db07f03f0d1ab265
Noah Wilson <noahwilsonmailbox32@gmail.com>
2021-09-10 10:21:48 -0400

    Movement

---
+++ KingsQueens/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    public float moveSpeed = 5f;
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        controller.SimpleMove(movement * moveSpeed);
+        
+    }
+}
